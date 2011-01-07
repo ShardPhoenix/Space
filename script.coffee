b6c08769dcd7bbb9b060518fe73bf6ddfbd8c403
Ryan Sattler <xgravityx@hotmail.com>
2011-01-07 22:58:29 +1100

    Missile max range implemented

--- script.coffee
+++ script.coffee
@@ -9,9 +9,24 @@ keys =
     UP: 38
     RIGHT: 39
     DOWN: 40
+    SPACE: 32
+    CTRL: 17
+    
+colors =
+    RED: "rgba(255,0,0,1.0)"
+    BLUE: "rgba(0,0,255,1.0)"
     
 input =
     keysHeld: []
+    
+state = 
+    ACTIVE: 0
+    DYING: 1
+    DEAD: 2
+    
+utils =
+    degToRad: (degrees) ->
+        0.0174532925 * degrees
 
 class Renderer
     constructor: ->
@@ -23,54 +38,118 @@ class Renderer
         
     clear: ->
         this.drawRect(0, 0, constants.WIDTH, constants.HEIGHT, "rgba(255,255,255,1.0)")
+        
+    renderShip: (ship) ->
+        @ctx.save()
+        @ctx.translate(ship.coord.x, ship.coord.y)
+        @ctx.rotate(0.0174532925 * ship.heading)
+        this.drawRect(-ship.width/2, -ship.length/2, ship.width, ship.length, ship.color)
+        @ctx.restore()
     
     render: (model) ->
-        this.clear()
-        @ctx.save()
-        @ctx.translate(model.player.pos[0], model.player.pos[1])
-        @ctx.rotate(0.0174532925 * model.player.heading)
-        this.drawRect(-model.player.width/2, -model.player.length/2, model.player.width, model.player.length, "rgba(255,0,0,1.0)")
-        @ctx.restore()
-
+        this.clear()      
+        this.renderShip(ship) for ship in model.ships
+        this.renderShip(bullet) for bullet in model.bullets
+        this.renderShip(model.player)
+        
+class Rocket
+    constructor: (coord, heading) ->
+        @distTravelled = 0
+        @maxDist = 300.0
+        @damage = 100
+        @radius = 100
+        @speed = 400.0
+        @coord = coord
+        @heading = heading
+        @color = colors.RED
+        @width = 5
+        @length = 10
+        @state = state.ACTIVE
+    
+    update: (dt) ->
+        if @distTravelled < @maxDist
+            dist = @speed * dt/1000.0
+            theta = utils.degToRad(@heading)
+            dx = dist * Math.sin(theta)
+            dy = dist * Math.cos(theta)
+            @coord.x += dx
+            @coord.y -= dy
+            @distTravelled += Math.sqrt(dx * dx + dy * dy)
+        else
+            #TODO: explode
+            @state = state.DEAD
+        
+        
 class Player
     constructor: ->
         @hp = 100
-        @heading = 135.0 # degrees, up = 0
-        @pos = [200, 200] # x, y
-        @speed = 3.0
-        @rot_speed = 90.0 #degrees per second
+        @heading = 135.0 #degrees, up = 0
+        @coord = x: 200, y: 200 #x, y
+        @speed = 200.0 #pix per second
+        @rotSpeed = 180.0 #degrees per second
         @width = 20
         @length = 40
+        @color = colors.RED
+        @state = state.ACTIVE
         
-    update: ->
-        #TODO: do these in terms of time instead of frame
+    update: (dt, model) ->
         if input.keysHeld[keys.UP]
-            theta = 0.0174532925 * @heading
-            @pos[0] += @speed * Math.sin(theta)
-            @pos[1] -= @speed * Math.cos(theta)
-            
+            dist = @speed * dt/1000.0
+            theta = utils.degToRad(@heading)
+            @coord.x += dist * Math.sin(theta)
+            @coord.y -= dist * Math.cos(theta)            
         if input.keysHeld[keys.LEFT]
-            @heading -= 5.0
+            @heading -= @rotSpeed * dt/1000.0
         if input.keysHeld[keys.RIGHT]
-            @heading += 5.0
+            @heading += @rotSpeed * dt/1000.0
+        if input.keysHeld[keys.CTRL]
+            model.bullets.push(new Rocket({x: @coord.x, y: @coord.y}, @heading))
         
+class Ship
+    constructor: (coord) ->
+        @hp = 50
+        @heading = 0.0
+        @coord = coord
+        @width = 20
+        @length = 40
+        @color = colors.BLUE
+        @state = state.ACTIVE
         
+    update : (dt) ->
+        
+
 class Model
     constructor: ->
         @player = new Player
+        @ships = (new Ship({x: Math.random() * constants.WIDTH, y: Math.random() * constants.HEIGHT}) for i in [1..10])
+        @bullets = []
 
-    update: ->
-        @player.update()
+    update: (dt) ->
+        @player.update(dt, this)
+        ship.update(dt) for ship in @ships
+        for bullet in @bullets
+            if bullet
+                bullet.update(dt) 
         
-
+        @ships = for ship in @ships
+            if ship.state != state.DEAD
+                ship
+        
+        @bullets = (bullet for bullet in (bullet for bullet in @bullets when bullet.state != state.DEAD) when bullet)
 
 class Controller
     constructor: ->
         @model = new Model
         @renderer = new Renderer
+        @lastTime = (new Date).getTime()
 
     tick: ->
-        @model.update()
+        d = new Date();
+        time = d.getTime();
+        dt = time - @lastTime
+        @lastTime = time
+        
+        @model.update(dt)
         @renderer.render(@model)
             
    
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function() {
-  var Controller, Model, Player, Renderer, constants, controller, gameTick, input, keys;
+  var Controller, Model, Player, Renderer, Rocket, Ship, colors, constants, controller, gameTick, input, keys, state, utils;
   constants = {
     WIDTH: 1024,
     HEIGHT: 768,
@@ -9,10 +9,26 @@
     LEFT: 37,
     UP: 38,
     RIGHT: 39,
-    DOWN: 40
+    DOWN: 40,
+    SPACE: 32,
+    CTRL: 17
+  };
+  colors = {
+    RED: "rgba(255,0,0,1.0)",
+    BLUE: "rgba(0,0,255,1.0)"
   };
   input = {
     keysHeld: []
+  };
+  state = {
+    ACTIVE: 0,
+    DYING: 1,
+    DEAD: 2
+  };
+  utils = {
+    degToRad: function(degrees) {
+      return 0.0174532925 * degrees;
+    }
   };
   Renderer = (function() {
     function Renderer() {
@@ -25,48 +41,176 @@
     Renderer.prototype.clear = function() {
       return this.drawRect(0, 0, constants.WIDTH, constants.HEIGHT, "rgba(255,255,255,1.0)");
     };
-    Renderer.prototype.render = function(model) {
-      this.clear();
+    Renderer.prototype.renderShip = function(ship) {
       this.ctx.save();
-      this.ctx.translate(model.player.pos[0], model.player.pos[1]);
-      this.ctx.rotate(0.0174532925 * model.player.heading);
-      this.drawRect(-model.player.width / 2, -model.player.length / 2, model.player.width, model.player.length, "rgba(255,0,0,1.0)");
+      this.ctx.translate(ship.coord.x, ship.coord.y);
+      this.ctx.rotate(0.0174532925 * ship.heading);
+      this.drawRect(-ship.width / 2, -ship.length / 2, ship.width, ship.length, ship.color);
       return this.ctx.restore();
     };
+    Renderer.prototype.render = function(model) {
+      var bullet, ship, _i, _j, _len, _len2, _ref, _ref2;
+      this.clear();
+      _ref = model.ships;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        ship = _ref[_i];
+        this.renderShip(ship);
+      }
+      _ref2 = model.bullets;
+      for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
+        bullet = _ref2[_j];
+        this.renderShip(bullet);
+      }
+      return this.renderShip(model.player);
+    };
     return Renderer;
+  })();
+  Rocket = (function() {
+    function Rocket(coord, heading) {
+      this.distTravelled = 0;
+      this.maxDist = 300.0;
+      this.damage = 100;
+      this.radius = 100;
+      this.speed = 400.0;
+      this.coord = coord;
+      this.heading = heading;
+      this.color = colors.RED;
+      this.width = 5;
+      this.length = 10;
+      this.state = state.ACTIVE;
+    }
+    Rocket.prototype.update = function(dt) {
+      var dist, dx, dy, theta;
+      if (this.distTravelled < this.maxDist) {
+        dist = this.speed * dt / 1000.0;
+        theta = utils.degToRad(this.heading);
+        dx = dist * Math.sin(theta);
+        dy = dist * Math.cos(theta);
+        this.coord.x += dx;
+        this.coord.y -= dy;
+        return this.distTravelled += Math.sqrt(dx * dx + dy * dy);
+      } else {
+        return this.state = state.DEAD;
+      }
+    };
+    return Rocket;
   })();
   Player = (function() {
     function Player() {
       this.hp = 100;
       this.heading = 135.0;
-      this.pos = [200, 200];
-      this.speed = 3.0;
-      this.rot_speed = 90.0;
+      this.coord = {
+        x: 200,
+        y: 200
+      };
+      this.speed = 200.0;
+      this.rotSpeed = 180.0;
       this.width = 20;
       this.length = 40;
+      this.color = colors.RED;
+      this.state = state.ACTIVE;
     }
-    Player.prototype.update = function() {
-      var theta;
+    Player.prototype.update = function(dt, model) {
+      var dist, theta;
       if (input.keysHeld[keys.UP]) {
-        theta = 0.0174532925 * this.heading;
-        this.pos[0] += this.speed * Math.sin(theta);
-        this.pos[1] -= this.speed * Math.cos(theta);
+        dist = this.speed * dt / 1000.0;
+        theta = utils.degToRad(this.heading);
+        this.coord.x += dist * Math.sin(theta);
+        this.coord.y -= dist * Math.cos(theta);
       }
       if (input.keysHeld[keys.LEFT]) {
-        this.heading -= 5.0;
+        this.heading -= this.rotSpeed * dt / 1000.0;
       }
       if (input.keysHeld[keys.RIGHT]) {
-        return this.heading += 5.0;
+        this.heading += this.rotSpeed * dt / 1000.0;
+      }
+      if (input.keysHeld[keys.CTRL]) {
+        return model.bullets.push(new Rocket({
+          x: this.coord.x,
+          y: this.coord.y
+        }, this.heading));
       }
     };
     return Player;
   })();
+  Ship = (function() {
+    function Ship(coord) {
+      this.hp = 50;
+      this.heading = 0.0;
+      this.coord = coord;
+      this.width = 20;
+      this.length = 40;
+      this.color = colors.BLUE;
+      this.state = state.ACTIVE;
+    }
+    Ship.prototype.update = function(dt) {};
+    return Ship;
+  })();
   Model = (function() {
     function Model() {
+      var i;
       this.player = new Player;
+      this.ships = (function() {
+        var _results;
+        _results = [];
+        for (i = 1; i <= 10; i++) {
+          _results.push(new Ship({
+            x: Math.random() * constants.WIDTH,
+            y: Math.random() * constants.HEIGHT
+          }));
+        }
+        return _results;
+      })();
+      this.bullets = [];
     }
-    Model.prototype.update = function() {
-      return this.player.update();
+    Model.prototype.update = function(dt) {
+      var bullet, ship, _i, _j, _len, _len2, _ref, _ref2;
+      this.player.update(dt, this);
+      _ref = this.ships;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        ship = _ref[_i];
+        ship.update(dt);
+      }
+      _ref2 = this.bullets;
+      for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
+        bullet = _ref2[_j];
+        if (bullet) {
+          bullet.update(dt);
+        }
+      }
+      this.ships = (function() {
+        var _i, _len, _ref, _results;
+        _ref = this.ships;
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          ship = _ref[_i];
+          _results.push(ship.state !== state.DEAD ? ship : void 0);
+        }
+        return _results;
+      }).call(this);
+      return this.bullets = (function() {
+        var _i, _len, _ref, _results;
+        _ref = (function() {
+          var _i, _len, _ref, _results;
+          _ref = this.bullets;
+          _results = [];
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            bullet = _ref[_i];
+            if (bullet.state !== state.DEAD) {
+              _results.push(bullet);
+            }
+          }
+          return _results;
+        }).call(this);
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          bullet = _ref[_i];
+          if (bullet) {
+            _results.push(bullet);
+          }
+        }
+        return _results;
+      }).call(this);
     };
     return Model;
   })();
@@ -74,9 +218,15 @@
     function Controller() {
       this.model = new Model;
       this.renderer = new Renderer;
+      this.lastTime = (new Date).getTime();
     }
     Controller.prototype.tick = function() {
-      this.model.update();
+      var d, dt, time;
+      d = new Date();
+      time = d.getTime();
+      dt = time - this.lastTime;
+      this.lastTime = time;
+      this.model.update(dt);
       return this.renderer.render(this.model);
     };
     return Controller;
